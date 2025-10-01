eaa520be791bdd963350c1718a36cacddd75031b
ᗪᗩᖇK ✞Oᖇᗪ <85796566+Judson-web@users.noreply.github.com>
2025-10-01 12:28:51 +0530

    Add files via upload

---
+++ dist/lib/markdown.js
@@ -0,0 +1,29 @@
+"use strict";
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.escapeMarkdown = escapeMarkdown;
+function escapeMarkdown(text) {
+    return text
+        .replace(/\\/g, '\\\\')
+        .replace(/_/g, '\\_')
+        .replace(/\*/g, '\\*')
+        .replace(/\[/g, '\\[')
+        .replace(/\]/g, '\\]')
+        .replace(/\(/g, '\\(')
+        .replace(/\)/g, '\\)')
+        .replace(/~/g, '\\~')
+        .replace(/`/g, '\\`')
+        .replace(/>/g, '\\>')
+        .replace(/#/g, '\\#')
+        .replace(/\+/g, '\\+')
+        .replace(/-/g, '\\-')
+        .replace(/=/g, '\\=')
+        .replace(/\|/g, '\\|')
+        .replace(/\{/g, '\\{')
+        .replace(/\}/g, '\\}')
+        .replace(/\./g, '\\.')
+        .replace(/!/g, '\\!');
+}
